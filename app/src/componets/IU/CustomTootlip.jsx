b0a4c1f402fbe33e52bdefdd9d7bb5740c04b71d
Danil <yalexxer228666@mail.ru>
2025-09-16 20:46:47 +0300

    feat | label custom tootlip

--- app/src/componets/IU/CustomTootlip.jsx
+++ app/src/componets/IU/CustomTootlip.jsx
@@ -1,5 +1,8 @@
+import { useCrypto } from '../../context/crypto-context'
 
 const CustomTooltip = ({ active, payload, label }) => {
+        const { theme } = useCrypto()
+    
     if (active && payload && payload.length) {
         const formatter = new Intl.NumberFormat("en-US", {
             style: "currency",
@@ -8,7 +11,7 @@ const CustomTooltip = ({ active, payload, label }) => {
             maximumFractionDigits: 2,
         });
         return (
-                <p className="text-white truncate">
+                <p style={{color: theme ? 'white' : 'black'}}>
                     {label}: {formatter.format(payload[0].value)}
                 </p>
         );
